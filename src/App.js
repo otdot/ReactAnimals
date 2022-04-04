3ba11dbf1876228141dbb90a573a0657aef9f5e5
otdot <s2200126@BC-C02GR0BSQ6LR-MacBook-Air.local>
2022-04-04 13:16:35 +0300

    changed class methods names

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { animals } from "./animals.js";
 class App extends Component {
   state = { animals: animals };
 
-  addLike = (name) => {
+  addLikeHandler = (name) => {
     const updatedArr = this.state.animals.map((animal) => {
       if (name === animal.name) {
         animal.likes++;
@@ -15,7 +15,7 @@ class App extends Component {
     return this.setState(updatedArr);
   };
 
-  deleteCard = (name) => {
+  deleteHandler = (name) => {
     const updatedArr = this.state.animals.filter(
       (animal) => name !== animal.name
     );
@@ -39,8 +39,8 @@ class App extends Component {
           imgsrc={`http://source.unsplash.com/170x270/?animal/${animal.name}`}
           name={animal.name}
           count={animal.likes}
-          func={() => this.addLike(animal.name)}
-          func2={() => this.deleteCard(animal.name)}
+          func={() => this.addLikeHandler(animal.name)}
+          func2={() => this.deleteHandler(animal.name)}
         />
       );
     });
